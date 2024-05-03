3948ec0d89360498105203d945fa332f055ff5b3
bluurryy <164359728+bluurryy@users.noreply.github.com>
2024-05-03 23:18:21 +0200

    feat: add `Jitter::frequency`

--- src/cellular.rs
+++ src/cellular.rs
@@ -1,5 +1,14 @@
+use crate::private_prelude::*;
+
 #[derive(Debug, Clone, Copy)]
 pub struct Jitter<CellularNoise> {
     pub noise: CellularNoise,
     pub jitter: f32,
+}
+
+impl<CellularNoise> Jitter<CellularNoise> {
+    #[inline(always)]
+    pub const fn frequency(self, frequency: f32) -> Frequency<Self> {
+        Frequency { noise: self, frequency }
+    }
 }
