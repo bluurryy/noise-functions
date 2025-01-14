72910163367e60040d5036b7ee4c5f340914bffd
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-01-14 10:10:59 +0100

    doc: remove comment about FastNoiseLite

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 //!
 //! Check out the [live demo](https://bluurryy.github.io/noise-functions-demo/)!
 //!
-//! The implementation of these noise functions are from FastNoiseLite ([github](https://github.com/Auburn/FastNoiseLite)/[crate](https://docs.rs/fastnoise-lite/latest/fastnoise_lite/)).
-//!
 //! ## Examples
 //! ```
 //! use noise_functions::{ Perlin, CellDistance, OpenSimplex2s, Sample2, Sample3, NoiseFn };
