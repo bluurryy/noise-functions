18cdef947095a84c34ff29f399e224276d92554e
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-01-19 00:21:20 +0100

    feat: make `Improve2X` pub

--- src/modifiers/open_simplex_2.rs
+++ src/modifiers/open_simplex_2.rs
@@ -1,3 +1,3 @@
 //! OpenSimplex2 noise functions and combinators.
 
-pub use crate::open_simplex_2::{Improve3Xy, Improve3Xz, Improve4XyZw, Improve4Xyz, Improve4XyzXy, Improve4XyzXz};
+pub use crate::open_simplex_2::{Improve2X, Improve3Xy, Improve3Xz, Improve4XyZw, Improve4Xyz, Improve4XyzXy, Improve4XyzXz};
