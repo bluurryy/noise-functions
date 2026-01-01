4be4c1a7ce4f52710553de5c3a0789a404f9f07c
bluurryy <164359728+bluurryy@users.noreply.github.com>
2026-01-01 15:53:38 +0100

    fix: update usage of nightly doc features for config crate

--- crates/noise-functions-config/src/lib.rs
+++ crates/noise-functions-config/src/lib.rs
@@ -9,7 +9,7 @@
 )]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "nightly-simd", feature(portable_simd))]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 extern crate alloc;
 
